c33b6e75555d4a66cd17895d249526847dce0746
Nikhil Vijayakumar <NikhilVijayakumar@users.noreply.github.com>
2021-06-01 19:31:17 +0530

    graph

--- notebook/data/sql/Statistics.sql
+++ notebook/data/sql/Statistics.sql
@@ -1,10 +1,11 @@
-#Based on algorithm,type
-SELECT algorithm,type,sum(case when  status = 'loss' then 1 else 0 end) as loss,
+#Based on close_time
+SELECT close_time, sum(case when  status = 'loss' then 1 else 0 end) as loss,
        sum(case when status = 'profit' then 1 else 0 end) as profit,
        sum(case when status = 'open' then 1 else 0 end) as open,
        round(sum(case when  status = 'loss' then 1.0 else 0.0 end)/count(*),2) as loss_ratio,
-       round(sum(case when status = 'profit' then 1.0 else 0.0 end)/count(*),2)  as profit_ratio
-FROM Statistics GROUP by algorithm,type ORDER by profit_ratio desc
+       round(sum(case when status = 'profit' then 1.0 else 0.0 end)/count(*),2)  as profit_ratio,
+	   round(sum(close*number_of_shares -current*number_of_shares ),2)  as returns
+FROM Statistics GROUP by close_time ORDER by returns
 
 
 #Based on algorithm
@@ -12,7 +13,8 @@ SELECT algorithm,sum(case when  status = 'loss' then 1 else 0 end) as loss,
        sum(case when status = 'profit' then 1 else 0 end) as profit,
        sum(case when status = 'open' then 1 else 0 end) as open,
        round(sum(case when  status = 'loss' then 1.0 else 0.0 end)/count(*),2) as loss_ratio,
-       round(sum(case when status = 'profit' then 1.0 else 0.0 end)/count(*),2)  as profit_ratio
+       round(sum(case when status = 'profit' then 1.0 else 0.0 end)/count(*),2)  as profit_ratio,
+	   round(sum(close*number_of_shares -current*number_of_shares ),2)  as returns
 FROM Statistics GROUP by algorithm ORDER by profit_ratio desc
 
 #Based on symbol
@@ -20,63 +22,25 @@ SELECT symbol,type,sum(case when  status = 'loss' then 1 else 0 end) as loss,
        sum(case when status = 'profit' then 1 else 0 end) as profit,
        sum(case when status = 'open' then 1 else 0 end) as open,
        round(sum(case when  status = 'loss' then 1.0 else 0.0 end)/count(*),2) as loss_ratio,
-       round(sum(case when status = 'profit' then 1.0 else 0.0 end)/count(*),2)  as profit_ratio
+       round(sum(case when status = 'profit' then 1.0 else 0.0 end)/count(*),2)  as profit_ratio,
+	   round(sum(close*number_of_shares -current*number_of_shares ),2)  as returns
 FROM Statistics GROUP by symbol,type ORDER by profit_ratio desc
 
-
-#loss > 0.6 short on symbol
-SELECT symbol,type,sum(case when  status = 'loss' then 1 else 0 end) as loss,
+#loss
+SELECT close_time, sum(case when  status = 'loss' then 1 else 0 end) as loss,
        sum(case when status = 'profit' then 1 else 0 end) as profit,
        sum(case when status = 'open' then 1 else 0 end) as open,
        round(sum(case when  status = 'loss' then 1.0 else 0.0 end)/count(*),2) as loss_ratio,
-       round(sum(case when status = 'profit' then 1.0 else 0.0 end)/count(*),2)  as profit_ratio
-FROM Statistics GROUP by symbol,type HAVING loss_ratio>0.6 AND type = 'short' ORDER by loss_ratio desc
+       round(sum(case when status = 'profit' then 1.0 else 0.0 end)/count(*),2)  as profit_ratio,
+	   round(sum(close*number_of_shares -current*number_of_shares ),2)  as returns
+FROM Statistics GROUP by close_time having returns < 0  ORDER by returns
 
 
-#loss > 0.6 long on symbol
-SELECT symbol,type,sum(case when  status = 'loss' then 1 else 0 end) as loss,
+#day status
+SELECT close_time,current,close,number_of_shares,symbol,type,sum(case when  status = 'loss' then 1 else 0 end) as loss,
        sum(case when status = 'profit' then 1 else 0 end) as profit,
        sum(case when status = 'open' then 1 else 0 end) as open,
        round(sum(case when  status = 'loss' then 1.0 else 0.0 end)/count(*),2) as loss_ratio,
-       round(sum(case when status = 'profit' then 1.0 else 0.0 end)/count(*),2)  as profit_ratio
-FROM Statistics GROUP by symbol,type HAVING loss_ratio>0.6 AND type = 'long' ORDER by loss_ratio desc
-
-
-
-
-#loss > 0.6 symbol
-SELECT symbol,sum(case when  status = 'loss' then 1 else 0 end) as loss,
-       sum(case when status = 'profit' then 1 else 0 end) as profit,
-       sum(case when status = 'open' then 1 else 0 end) as open,
-       round(sum(case when  status = 'loss' then 1.0 else 0.0 end)/count(*),2) as loss_ratio,
-       round(sum(case when status = 'profit' then 1.0 else 0.0 end)/count(*),2)  as profit_ratio
-FROM Statistics GROUP by symbol HAVING loss_ratio>0.6  ORDER by loss_ratio desc
-
-
-
-#profit_ratio > 0.3  symbol
-SELECT symbol,sum(case when  status = 'loss' then 1 else 0 end) as loss,
-       sum(case when status = 'profit' then 1 else 0 end) as profit,
-       sum(case when status = 'open' then 1 else 0 end) as open,
-       round(sum(case when  status = 'loss' then 1.0 else 0.0 end)/count(*),2) as loss_ratio,
-       round(sum(case when status = 'profit' then 1.0 else 0.0 end)/count(*),2)  as profit_ratio
-FROM Statistics GROUP by symbol HAVING profit_ratio> 0.3  ORDER by profit_ratio desc
-
-
-
-#Common symbol, having > 0.6 loss on both long and short 
-SELECT symbol FROM Statistics WHERE  symbol in 
-(SELECT  q.symbol FROM (SELECT symbol,type,sum(case when  status = 'loss' then 1 else 0 end) as loss,
-       sum(case when status = 'profit' then 1 else 0 end) as profit,
-       sum(case when status = 'open' then 1 else 0 end) as open,
-       round(sum(case when  status = 'loss' then 1.0 else 0.0 end)/count(*),2) as loss_ratio,
-       round(sum(case when status = 'profit' then 1.0 else 0.0 end)/count(*),2)  as profit_ratio
-FROM Statistics GROUP by symbol,type HAVING loss_ratio>0.6 AND type = 'short' ORDER by loss_ratio desc) q) 
-AND symbol in
-(SELECT  q.symbol FROM (SELECT symbol,type,sum(case when  status = 'loss' then 1 else 0 end) as loss,
-       sum(case when status = 'profit' then 1 else 0 end) as profit,
-       sum(case when status = 'open' then 1 else 0 end) as open,
-       round(sum(case when  status = 'loss' then 1.0 else 0.0 end)/count(*),2) as loss_ratio,
-       round(sum(case when status = 'profit' then 1.0 else 0.0 end)/count(*),2)  as profit_ratio
-FROM Statistics GROUP by symbol,type HAVING loss_ratio>0.6 AND type = 'long' ORDER by loss_ratio desc) q)
-GROUP by symbol
+       round(sum(case when status = 'profit' then 1.0 else 0.0 end)/count(*),2)  as profit_ratio,
+	   round(sum(close*number_of_shares -current*number_of_shares ),2)  as returns
+FROM Statistics GROUP by symbol,close_time having close_time = '2021-05-09-11:44:13' ORDER by returns
