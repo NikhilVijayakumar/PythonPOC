55cef138e447ded73d58a284b7080396cbdd262f
Nikhil Vijayakumar <NikhilVijayakumar@users.noreply.github.com>
2021-06-07 16:31:46 +0530

    nikhil

--- notebook/data/sql/Statistics.sql
+++ notebook/data/sql/Statistics.sql
@@ -4,7 +4,7 @@ SELECT close_time, sum(case when  status = 'loss' then 1 else 0 end) as loss,
        sum(case when status = 'open' then 1 else 0 end) as open,
        round(sum(case when  status = 'loss' then 1.0 else 0.0 end)/count(*),2) as loss_ratio,
        round(sum(case when status = 'profit' then 1.0 else 0.0 end)/count(*),2)  as profit_ratio,
-	   round(sum(close*number_of_shares -current*number_of_shares ),2)  as returns
+	   round(sum(case when type = 'long' then close*number_of_shares -current*number_of_shares else current*number_of_shares -close*number_of_shares end),2) as returns
 FROM Statistics GROUP by close_time ORDER by close_time
 
 
@@ -14,7 +14,7 @@ SELECT algorithm,sum(case when  status = 'loss' then 1 else 0 end) as loss,
        sum(case when status = 'open' then 1 else 0 end) as open,
        round(sum(case when  status = 'loss' then 1.0 else 0.0 end)/count(*),2) as loss_ratio,
        round(sum(case when status = 'profit' then 1.0 else 0.0 end)/count(*),2)  as profit_ratio,
-	   round(sum(close*number_of_shares -current*number_of_shares ),2)  as returns
+	   round(sum(case when type = 'long' then close*number_of_shares -current*number_of_shares else current*number_of_shares -close*number_of_shares end),2) as returns
 FROM Statistics GROUP by algorithm ORDER by profit_ratio desc
 
 #Based on symbol
